d46b486788eed982fdf3b5e4af03cbf0b441664b
mwong <wongmt15@gmail.com>
2023-03-30 05:00:37 +1100

    30-3-23

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/mysite/",
+  base: "/myapp/",
   plugins: [vue()],
   resolve: {
     alias: {
