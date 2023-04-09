57e201fd4c43eefc8b035fdad3c441f037118744
mwong <wongmt15@gmail.com>
2023-04-10 05:43:49 +1000

    10-4-23

--- src/views/Photos.vue
+++ src/views/Photos.vue
@@ -1,18 +1,13 @@
-<script>
-export default {
-  data() {
-    return {
-      img: '/acupy.jpg',
-      count: 1
-    }
-  },
-  methods: {
-    select() {
-      this.count++
-      if (this.count > 3) 
-         {this.count = 1;}
-    }
-  }
+<script setup>
+import {ref} from 'vue'
+
+const img = ref('/acupy.jpg')
+const count = ref(1)
+
+function select() {
+   count.value++
+   if (count.value > 3) 
+      count.value = 1
 }
 </script>
 
@@ -22,11 +17,11 @@ export default {
    <button @click="select"><b>Click to select next photo</b></button>
    <br />
    <h4 v-if="count==1">Australian Catholic University, Strathfield Campus, NSW, Australia</h4>
-   <img v-if="count==1" src="\acupy.jpg" alt="acu">
+   <img v-if="count==1" src="/acupy.jpg" alt="acu">
    <h4 v-if="count==2">University of Technology Sydney, Building 5, Sydney, Australia</h4>
-   <img v-if="count==2" src="\uts-bldg5.jpg" alt="acu">
+   <img v-if="count==2" src="/uts-bldg5.jpg" alt="uts">
    <h4 v-if="count==3">Homebush Public School, Homebush, NSW, Australia</h4>
-   <img v-if="count==3" src="\hps.jpg" alt="acu">   
+   <img v-if="count==3" src="/hps.jpg" alt="hps">   
 </div>
 </template>
 
