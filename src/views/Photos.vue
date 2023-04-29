7a99b8d14ffef84fc22a41a9fe943070a2fdfe49
M Wong <wongmt15@gmail.com>
2023-04-30 05:59:43 +1000

    30-4-23

--- src/views/Photos.vue
+++ src/views/Photos.vue
@@ -1,13 +1,22 @@
 <script setup>
 import {ref} from 'vue'
 
-const img = ref('/acupy.jpg')
-const count = ref(1)
+const count = ref(0)
+const photos = ref([
+		{  desc: 'Australian Catholic University, Strathfield Campus, NSW, Australia',
+		   image: '/acupy.jpg',
+		   alt: 'acu'},
+		{  desc: 'University of Technology Sydney, Building 5, Sydney, Australia',
+		   image: '/uts-bldg5.jpg',
+		   alt: 'uts'},
+		{  desc: 'Homebush Public School, Homebush, NSW, Australia',
+		   image: '/hps.jpg',
+		   alt: 'hps'}])		 
 
 function select() {
    count.value++
-   if (count.value > 3) 
-      count.value = 1
+   if (count.value > 2) 
+      count.value = 0
 }
 </script>
 
@@ -16,12 +25,9 @@ function select() {
    <h1>Photos</h1>
    <button @click="select"><b>Click to select next photo</b></button>
    <br />
-   <h4 v-if="count==1">Australian Catholic University, Strathfield Campus, NSW, Australia</h4>
-   <img v-if="count==1" src="/acupy.jpg" alt="acu">
-   <h4 v-if="count==2">University of Technology Sydney, Building 5, Sydney, Australia</h4>
-   <img v-if="count==2" src="/uts-bldg5.jpg" alt="uts">
-   <h4 v-if="count==3">Homebush Public School, Homebush, NSW, Australia</h4>
-   <img v-if="count==3" src="/hps.jpg" alt="hps">   
+   <h4>{{photos[count].desc}}</h4>
+   <img :src="photos[count].image" :alt="photos[count].alt">
+   
 </div>
 </template>
 
