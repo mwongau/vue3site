5d03c52b967707931d1a62c6d936048ce6c6cfe0
M Wong <wongmt15@gmail.com>
2025-07-25 07:52:34 +1000

    25-07-25

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <main>
     <h2>Home</h2>
 	<div class="Intro">
-	<p>Welcome to the personal site of M Wong. I am a researcher in computer vision & machine 
+	<p>Welcome to the personal site of M. Wong. I am a researcher in computer vision & machine 
 	learning with Ph.D in Computer Science, M.Sc & B.Sc(Hons.) in Electrical and Electronic 
 	Engineering. I also have experience in web app development. This site is built by using 
 	Vue 3, Vite, JavaScript, HTML and CSS. This site is hosted by GitHub Pages.</p>
